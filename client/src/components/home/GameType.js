ef9f6e3b851d53bb8963f5cfac5fff115c5c9eb3
ZeyXs <basile.gamer@gmail.com>
2024-03-12 15:43:24 +0100

    Added bg img to create game form

--- client/src/components/home/GameType.js
+++ client/src/components/home/GameType.js
@@ -1,8 +1,8 @@
-const GameType = ({ type, title, isSelected, handleGameType }) => {
+const GameType = ({ type, title, isSelected, handleGameType, bgUrl }) => {
     return (
-        <div onClick={() => handleGameType(type)} className={`rounded-lg border-2 ${isSelected ? 'border-red-500' : 'border-white'} relative overflow-hidden`}>
-            <img src="https://media.istockphoto.com/id/952007312/vector/card-games-flat-design-western-icon.jpg?s=612x612&w=0&k=20&c=Y2b2g4eZrP0Wy6B5lIcdJhkTZxVlXzRewjPrhdh5vms=" className="rounded-lg h-[70px] w-[110px] object-cover" />
-            <div className="absolute box-content bottom-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center text-white opacity-0 transition-opacity duration-300 ease-in-out hover:opacity-100">{title}</div>
+        <div onClick={() => handleGameType(type)} className={`rounded-lg z-10 border-spacing-0 border-2 ${isSelected ? 'border-purple-500' : 'border-white'} relative overflow-hidden cursor-pointer`}>
+            <img src={bgUrl} className="h-[70px] w-[110px] object-cover" />
+            <div className="absolute box-content bottom-0 left-0 w-full h-full bg-black bg-opacity-60 flex items-center justify-center text-white font-bold opacity-0 transition-opacity duration-200 ease-in-out hover:opacity-100">{title}</div>
         </div>
     );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,9 +3,8 @@ module.exports = {
     content: ['./src/**/*.{html,js}'],
     theme: {
         extend: {
-            textShadow: {
-                default: '0 2px 5px rgba(0, 0, 0, 0.5)',
-                lg: '0 2px 10px rgba(0, 0, 0, 0.5)',
+            fontFamily: {
+                custom: ['Jost', 'sans-serif'],
             },
         },
     },
