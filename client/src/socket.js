856ce0206bf29e8c3ffea85864ac4d519174cc2c
ZeyXs <basile.gamer@gmail.com>
2024-04-10 12:45:13 +0200

    Dockerizing the web app

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
+const URL = import.meta.env.PROD ? "https://plateau-api.zeyx.dev" : 'http://localhost:4000';
 
 export const socket = io(URL);
