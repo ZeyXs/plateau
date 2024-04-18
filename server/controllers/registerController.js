a4245e45ed109f089f530a3ba15bd452373795ec
ZeyXs <basile.gamer@gmail.com>
2024-04-18 16:00:14 +0200

    Fixed milleBornes error typo which caused NaN in leaderboard

--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -39,7 +39,7 @@ const handleNewUser = async (req, res) => {
                     "loses":0,
                     "avgPointsPerGame": 0
                 },
-                "millesBornes": {
+                "milleBornes": {
                     "gamesPlayed": 0,
                     "wins":0,
                     "loses":0
